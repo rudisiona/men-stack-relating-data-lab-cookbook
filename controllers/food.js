9480650de25d14182ed86141fc152217c4df1094
rudisiona <rsmageo@gmail.com>
2025-06-25 20:55:08 -0700

    build new

--- controllers/food.js
+++ controllers/food.js
@@ -5,6 +5,10 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('foods/index.ejs');
   });
+
+router.get('/users/:userId/foods/new', (req, res) => {
+    res.render('new.ejs')
+})
   
 const User = require('../models/user.js');
 
