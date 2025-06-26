ee73961b6b9883cb59b57346dae7f9f74d710d98
rudisiona <rsmageo@gmail.com>
2025-06-25 19:31:37 -0700

    index route

--- controllers/food.js
+++ controllers/food.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.get('/', (req, res) => {
+    res.render('foods/index.ejs');
+  });
+  
 const User = require('../models/user.js');
 
-module.export = router;
+module.exports = router;
