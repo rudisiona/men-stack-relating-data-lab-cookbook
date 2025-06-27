b1c5b57d7b9efeaacd31ff39a93a0263d4da6dff
rudisiona <rsmageo@gmail.com>
2025-06-27 16:53:56 -0700

    delete functionality

--- controllers/food.js
+++ controllers/food.js
@@ -7,7 +7,9 @@ router.get('/users/:userId/foods', async (req, res) => {
 
     if (!currentUser) throw new Error('User not found');
 
-    res.render('foods/index.ejs', { pantry: currentUser.pantry });
+    res.render('foods/index.ejs', { pantry: currentUser.pantry,
+      userId: currentUser._id.toString()
+     });
   } catch (error) {
     console.log(error);
     res.redirect('/');
@@ -38,6 +40,19 @@ router.post('/users/:userId/foods', async (req, res) => {
     console.log(error);
     res.redirect('/')
   }
+})
+
+router.delete('/users/:userId/foods/:foodId', async (req, res) => {
+  try {const currentUser = await User.findById(req.session.user._id);
+    currentUser.pantry.remove(req.params.foodId)
+    await currentUser.save();
+    console.log('Saved pantry:', currentUser.pantry )
+    res.redirect(`/users/${currentUser._id}/foods`)
+  
+    } catch (error){
+      console.log(error);
+      res.redirect('/')
+    }
 })
 
 const User = require('../models/user.js');
