1bf5a6013f30d8664f2634281275d048e0842502
rudisiona <rsmageo@gmail.com>
2025-06-27 16:36:08 -0700

    show

--- controllers/food.js
+++ controllers/food.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/users/:userId/foods', (req, res) => {
-  const userId = req.params.userId;
-  res.render('foods/index.ejs', { userId });
+router.get('/users/:userId/foods', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+
+    if (!currentUser) throw new Error('User not found');
+
+    res.render('foods/index.ejs', { pantry: currentUser.pantry });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
 });
 
 router.get('/users/:userId/foods/new', (req, res) => {
-  res.render('new.ejs');
+  const userId = req.params.userId
+  res.render('new.ejs', { userId });
 });
+
+
+
+
+router.post('/users/:userId/foods', async (req, res) => {
+  try {const currentUser = await User.findById(req.session.user._id);
+  currentUser.pantry.push({
+    foodGroup: req.body.foodGroup,
+    name: req.body.name,
+    readyToEat: req.body.readyToEat === 'on',
+    notes: req.body.notes
+  });
+  await currentUser.save();
+  console.log('Saved pantry:', currentUser.pantry )
+  res.redirect(`/users/${currentUser._id}/foods`)
+
+  } catch (error){
+    console.log(error);
+    res.redirect('/')
+  }
+})
 
 const User = require('../models/user.js');
 
